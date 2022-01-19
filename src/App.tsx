16261ea6490e584f71bb616a0d54f01f6d362976
Atharv <atharvbhandare2001@gmail.com>
2022-01-19 14:39:09 +0530

    useContext-future-value

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
 import "./App.css";
-import { Container } from "./components/Container";
-import PersonList from "./components/PersonList";
-import { Counter } from "./components/state/Counter";
-import { User } from "./components/state/User";
-import { Box } from "./contexts/Box";
-import { ThemeContextProvider } from "./contexts/ThemeContext";
+import User from "./contexts/User";
+import { UserContextProvider } from "./contexts/UserContext";
 
-function App() {
-  const nameList = [
-    {
-      first: "Atharv",
-      last: "Bhandare",
-      id: 1,
-    },
-    {
-      first: "Virat",
-      last: "Kohli",
-      id: 2,
-    },
-    {
-      first: "Sachin",
-      last: "Tendulkar",
-      id: 3,
-    },
-  ];
+export default function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Box />
-        <Container styles={{ padding: "2rem" }} />
-        <PersonList names={nameList} />
+      <UserContextProvider>
         <User />
-        <Counter />
-      </ThemeContextProvider>
+      </UserContextProvider>
     </div>
-  );
+  )    
 }
 
-export default App;
+
+
---
+++ src/contexts/User.tsx
@@ -0,0 +1,23 @@
+import { useContext } from "react"
+import { UserContext } from "./UserContext"
+
+export default function User() {
+    const userContext = useContext(UserContext)
+    const handleLogin = () => {
+         userContext.setUser({
+                name:'Atharv',
+                email: 'atharv@gmail.com',
+            })
+    }
+    const handleLogout = () => {
+            userContext.setUser(null)
+    }
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <div>User name is: {userContext.user?.name} </div>
+            <div>User email is: {userContext.user?.email} </div> 
+        </div>
+    )
+}
